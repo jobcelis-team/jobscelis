bdfbdd16c347d2823359dc145aa294eb512041ec
vladimirceli <bladimirtutoriales@gmail.com>
2026-03-07 00:12:12 -0500

    fix: bump Python SDK to 1.4.1, update author email to support@jobcelis.com

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="jobcelis",
-    version="1.4.0",
+    version="1.4.1",
     description="Official Python SDK for the Jobcelis Event Infrastructure Platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
